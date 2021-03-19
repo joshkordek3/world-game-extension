21bf4231497e9b867c6a6f612b87d90d00591ce1
joshkordek3 <79750252+joshkordek3@users.noreply.github.com>
2021-03-19 20:46:11 +0800

    fixed naming

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 //% color=#088530 weight=50 icon="\uf11b" block="Game"
-namespace game.world {
+namespace game_world {
 //% block="move $leftrightupdown left/right/up/down by $steps"
 //% group="Moving"
 export function move_by_ (leftrightupdown: string, steps: number) {
