7e90729890eb402705005275a46b2f47d9fc3fdc
joshkordek3 <79750252+joshkordek3@users.noreply.github.com>
2021-03-21 19:10:26 +0800

    added a spawn point

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@
 namespace World {
 //% block="move $leftrightupdown left/right/up/down by $steps"
 //% group="Moving"
+//% steps.min=0 steps.max=108
 export function move (leftrightupdown: string, steps: number) {
     if (leftrightupdown == "left") {
         leftright_difference += steps
@@ -16,6 +17,7 @@ export function move (leftrightupdown: string, steps: number) {
 }
 //% block="move $leftrightupdown and $leftrightupdown2 by $steps"
 //% group="Moving"
+//% steps.min=0 steps.max=108
 export function move_ (leftrightupdown: string, leftrightupdown2: string, steps: number) {
     move(leftrightupdown, steps)
     move(leftrightupdown2, steps)
@@ -39,8 +41,29 @@ export function x_pos () {
 export function y_pos () {
     return 2 - updown_difference
 }
+//% block="spawnpoint myself at the current position"
+//% group="Position"
+export function spawnpoint () {
+    spawn_x = x_pos()
+    spawn_y = y_pos()
+}
+//% block="set spawnpoint to x: $x y: $y"
+//% group="Position"
+//% x.min=-9 x.max=99
+//% y.min=-9 y.max=99
+export function spawnpoint_at_pos (x: number, y: number) {
+    spawn_x = x
+    spawn_y = y
+}
+//% block="go to spawnpoint"
+//% group="Position"
+export function spawn () {
+    goto(spawn_x, spawn_y)
+}
 //% block="go to x: $x y: $y"
 //% group="Position"
+//% x.min=-9 x.max=99
+//% y.min=-9 y.max=99
 export function goto (x: number, y: number) {
     updown_difference = 2 - y
     leftright_difference = 2 - x
@@ -53,6 +76,8 @@ export function world_blocks_detect () {
 }
 //% block="is there a block at x: $x_pos y: $y_pos"
 //% group="Detect"
+//% x_pos.min=-9 x_pos.max=99
+//% y_pos.min=-9 y_pos.max=99
 export function block_detect (x_pos: number, y_pos: number) {
     temp_txt = convertToText(x_pos)
     temp_txt2 = convertToText(y_pos)
@@ -66,21 +91,25 @@ export function block_detect (x_pos: number, y_pos: number) {
 }
 //% block="world $xy (x/y) coordinate at item $place (min=1)"
 //% group="Position"
+//% place.min=1 place.max=11664
 export function world_blocks (xy: string, place: number) {
     if (xy = "x") {
         return parseFloat(world[place - 1 ].substr(0, 2))
     } else if (xy = "y") {
         return parseFloat(world[place - 1].substr(2, 2)) 
     }
-    return 0
+    return -10
 }
 //% block="world (array)"
 //% group="Position"
 export function all_world_blocks () {
     return world
 }
-//% block="add by $columnrow (column/row) from $from_xy to $to_xy on $xy"
+//% block="add by $columnrow (column/row) at $xy from $from_xy to $to_xy "
 //% group="Creating"
+//% xy.min=-9 xy.max=99
+//% from_xy.min=-9 from_xy.max=99
+//% to_xy.min=-9 to_xy.max=99
 export function add_by (columnrow: string, from_xy: number, to_xy: number, xy: number) {
     if (columnrow == "column") {
         for (let index2 = 0; index2 <= to_xy - from_xy; index2++) {
@@ -94,6 +123,8 @@ export function add_by (columnrow: string, from_xy: number, to_xy: number, xy: n
 }
 //% block="add x $x and y $y to the world"
 //% group="Creating"
+//% x.min=-9 x.max=99
+//% y.min=-9 y.max=99
 export function add (x: number, y: number) {
     temp_txt = convertToText(x)
     temp_txt2 = convertToText(y)
@@ -111,4 +142,7 @@ let world: string[] = []
 //let world2: boolean[][] = [[]];
 let updown_difference = 0
 let leftright_difference = 0
+let spawn_x = 0
+let spawn_y = 0
+goto(spawn_x, spawn_y)
 }
